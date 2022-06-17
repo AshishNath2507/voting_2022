9384b439b8ae0023280df5a5418c3299702c94c7
Ashish <ashishnath098@gmail.com>
2022-06-17 11:47:46 +0530

    Update cand_home.php

--- users/candidate/cand_home.php
+++ users/candidate/cand_home.php
@@ -8,11 +8,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Candidate Homepage</title>
+    <!-- Bootstrap links -->
+    <link rel="stylesheet" href="../library/css/bootstrap.min.css">
+    <style>
+    @font-face { font-family: anurati; src: url('./library/fonts/anurati/ANURATI/Anurati-Regular.otf'); } 
+    .anurati {
+        font-family: anurati;
+    }
+</style>
 </head>
 <body>
-    <p>Candidate Homepage</p>
+    <p class="fs-1">Candidate Homepage</p>
     <?php
         echo $_SESSION['candidate'];
     ?>
+    <!-- Bootstrap script -->
+    <script src="../library/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
